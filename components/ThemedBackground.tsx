e38b8796b0ec15ef5440e40a95b9c4a8314c874e
ColeZap <cjshuman12@gmail.com>
2025-11-06 20:37:35 -0500

    themed background change

--- components/ThemedBackground.tsx
+++ components/ThemedBackground.tsx
@@ -1,17 +1,27 @@
-// components/ThemedBackground.tsx
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
+import { View } from "react-native";
 
 export function ThemedBackground({ children }: { children: React.ReactNode }) {
   return (
-    <LinearGradient
+    <View style={{ flex: 1, backgroundColor: "#000" }}>
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        colors={["#1a1a1aff", "#2b2a2aff"]}
+        style={{ flex: 1 }}
+      >
+        {children}
+      </LinearGradient>
+    </View>
+  );
+}
+
+{
+  /* <LinearGradient
       colors={["#2b2a2aff", "#141414ff"]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
       locations={[0, 1]}
       style={{ flex: 1 }}
-    >
-      {children}
-    </LinearGradient>
-  );
+    ></LinearGradient> */
 }
